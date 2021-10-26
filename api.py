bc44641d41202cdeea620b3d9b59af4218816fbc
Joaquin Jimenez Garcia <garcia.jjimenez@gmail.com>
2021-10-26 19:46:46 +0200

    Add function to get data from a given url

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 import json
 import os
+from urllib.request import Request, urlopen, ssl, socket
+from urllib.error import URLError, HTTPError
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/<user>')
 def index(user):
@@ -16,5 +20,34 @@ def index(user):
     # Closing file
     f.close()
     return data
+
+@app.route('/site/<site>')
+def site(site):
+    print("Checking " + site)
+    base_url = site
+    port = '443'
+    hostname = base_url
+    context = ssl.create_default_context()
+    site_result = []
+    try:
+        with socket.create_connection((hostname, port)) as sock:
+            with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+                version = ssock.version()
+                data = json.dumps(ssock.getpeercert())
+                data = json.loads(data)
+                site_result.append(site)
+                site_result.append(version)
+                site_result.append(data['notBefore'])
+                site_result.append(data['notAfter'])
+    except socket.error as err:
+        print()
+        site_result.append(site)
+        site_result.append("0")
+        site_result.append("0")
+        site_result.append("0")
+
+    site_result = json.dumps(site_result)  
+    
+    return site_result
 
 app.run(host='0.0.0.0', port=81)
